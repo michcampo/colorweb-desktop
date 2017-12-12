b4805199ab40e81fab6491eae0cbc8f7e8e218a2
Michele <crezi.camp@gmail.com>
2017-12-12 19:33:58 +0100

    s

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,11 @@ import { NgForm } from "@angular/forms/public_api";
 })
 export class HomeComponent implements OnInit {
   title = `Inserimento nuovo colore`;
+  controlloQualita = true;
+  risultatoQualita = false;
+  nuovoColore = false;
+  primoScan;
+  secondoScan;
  indice = 0;
  nometb;
  sottocm;
@@ -19,6 +24,12 @@ export class HomeComponent implements OnInit {
  codiceR;
  codiceG;
  codiceB;
+ codiceR1;
+ codiceG1;
+ codiceB1;
+ codiceR2;
+ codiceG2;
+ codiceB2;
  deltaE;
  avviso;
  codi;
@@ -42,7 +53,7 @@ codiceNeutroList = [];
 public listaCodici = [];
 confronto = false;
 
-
+qualita;
 
   constructor(private http: Http) { }
 public cmp = []
@@ -146,7 +157,8 @@ prendiColore(){
    if(codiceRGB.length != 0){
        this.rgbString = "R: " + codiceRGB[0] + " G: " + codiceRGB[1] + " B: " + codiceRGB[2];
       //da sistemare il rettangolino
-       this.rGb = "rgb("+ codiceRGB[0] + ", " +  codiceRGB[1] + "," +  codiceRGB[2] + " )";
+      this.rGb = "rgb("+ Math.round(codiceRGB[0]) + ", " +  Math.round(codiceRGB[1]) + "," +  Math.round(codiceRGB[2]) + ")";
+       console.log("RGS" + this.rGb);
        this.rgbPres = codiceRGB;
        this.data = codici[0].data;
        this.mostraConfronto = true;
@@ -159,6 +171,18 @@ prendiColore(){
   
 
 
+negativo(){
+  this.codiceR = "";
+  this.codiceG = "";
+  this.codiceB = "";
+  this.rgbScan = ""; 
+  this.confronto = false;
+  this.deltaE = null;
+  this.lab2 = [];
+  this.mostraConfronto = true;
+}
+
+
  prendiDeltaE(value: any){
   this.codiceR = value.codiceR;
   this.codiceG = value.codiceG;
@@ -169,7 +193,9 @@ prendiColore(){
   var r2 = Number(this.codiceR);
   var g2 = Number(this.codiceG);
   var b2 = Number(this.codiceB);
-  this.rgbScan = "rgb("+ r2 + ", " +  g2 + "," +  b2 + " )";
+  
+  this.rgbScan = "rgb("+ Math.round(r2) + ", " +  Math.round(g2) + ", " +  Math.round(b2) + ")";
+  console.log(typeof(r2));
   this.lab1 = this.rgb2Lab(this.rgbPres[0], this.rgbPres[1], this.rgbPres[2]);
   this.lab2 = this.rgb2Lab(r2, g2, b2);
   this.deltaE = this.getDeltaE(this.lab1, this.lab2);
@@ -194,7 +220,12 @@ cambia($event){
       }
 }
 
+random_rgb() {
+  var o = Math.round, r = Math.random, s = 255;
 
+  var rgb =[o(r()*s),o(r()*s),o(r()*s)];
+  return rgb;
+}
 
 rgb2Lab(r: number, g: number, b: number){
   var rgb = [r, g, b];
@@ -298,10 +329,11 @@ getDeltaE(lb, la) {
 
 onSubmit(form: any): void {  
   this.avviso= "";
+  console.log("Ciao");
   var param = {"tabella": this.nometb, "sottocm": this.sottocm, "idmaster": this.idMaster, "codiceCol":this.codiceCol};
  console.log("Codice: "+ form.codiceColore+ " R: "+ form.codiceR+ " G:"+ form.codiceG+ " B: "+form.codiceB + "Materiale: "+form.group1);
 //fai la funzione per l'inserimento
-// this.postsService.inserisciPelle( form.codiceR, form.codiceG, form.codiceB, form.note, param);
+this.inserisciPelle( form.codiceR, form.codiceG, form.codiceB, form.note, param);
 this.avviso = "Il colore  RGB("+form.codiceR+","+form.codiceG+","+form.codiceB+") è stato aggiunto correttamente."
 this.codiceR = "";
 this.codiceG = "";
@@ -318,8 +350,8 @@ inserimento(){
   this.avviso= "";
   var param = {"tabella": this.nometb, "sottocm": this.sottocm, "idmaster": this.idMaster, "codiceCol":this.codiceCol};
  console.log(" R: "+ this.codiceR+ " G:"+ this.codiceG+ " B: "+this.codiceB);
- //fai la funzione
-//this.postsService.inserisciPelle( this.codiceR, this.codiceG, this.codiceB, this.note, param);
+ 
+this.inserisciPelle( this.codiceR, this.codiceG, this.codiceB, this.note, param);
 this.avviso = "Il colore  RGB("+this.codiceR+","+this.codiceG+","+this.codiceB+") è stato aggiunto correttamente."
 this.codiceR = "";
 this.codiceG = "";
@@ -347,13 +379,14 @@ this.lab1 = [];
 
 }
 
-
+idx = 1;
 
 
 getColoriDaSensore(){
   this.codiceR = "";
 this.codiceG = "";
 this.codiceB = "";
+
 /*
   this.http.get('http://localhost:8080/sensore')
 .subscribe(
@@ -372,11 +405,66 @@ this.codiceB = b;
 
 
 });*/
-this.codiceR = 255;
-this.codiceG = 21;
-this.codiceB = 234;
+var rgbRandom = this.random_rgb();
+this.codiceR = rgbRandom[0].toString();
+this.codiceG = rgbRandom[1].toString();
+this.codiceB = rgbRandom[2].toString();
+
+
+
 }
 
+
+getColori2(){
+
+this.codiceR2 = "";
+this.codiceG2 = "";
+this.codiceB2 = "";
+  var rgbRandom = this.random_rgb();
+  this.codiceR2= rgbRandom[0].toString();
+  this.codiceG2 = rgbRandom[1].toString();
+  this.codiceB2 = rgbRandom[2].toString();  
+
+}
+
+
+getColori1(){
+
+
+  this.codiceR1 = "";
+  this.codiceG1 = "";
+  this.codiceB1 = "";
+  var rgbRandom = this.random_rgb();
+  this.codiceR1 = rgbRandom[0].toString();
+  this.codiceG1 = rgbRandom[1].toString();
+  this.codiceB1 = rgbRandom[2].toString();  
+  
+}
+
+quality(){
+  var array1 =[this.codiceR1, this.codiceG1,  this.codiceB1];
+  var array2 =[this.codiceR2, this.codiceG2,  this.codiceB2];
+  this.qualita = this.getDeltaE(array1, array2);
+
+this.primoScan = "rgb("+ Math.round(this.codiceR1) + ", " +  Math.round(this.codiceG1) + "," +  Math.round(this.codiceB1) + ")";
+this.secondoScan = "rgb("+ Math.round(this.codiceR2) + ", " +  Math.round(this.codiceG2) + "," +  Math.round(this.codiceB2) + ")";
+this.risultatoQualita = true;
+this.controlloQualita = false;
+console.log(this.risultatoQualita);
+}
+  //inserisce il materiale
+  inserisciPelle(r1: any, g1: any, b1: any, nota: String, tab: Object) {
+    console.log("" + r1 + "" + g1 + "" + b1);
+    if (r1 > 255 || g1 > 255 || b1 > 255) {
+
+      return window.alert("Attenzione, sono stati inseriti valori non validi.");
+    } else {
+      var riga = [];
+      const body = { r: r1, g: g1, b: b1, tabella: tab, note: nota, riga: riga };
+      let _url: string = 'http://localhost:8080/inserisci';
+      return this.http.post(_url, body).subscribe();
+    }
+  }
 
 
 indietro(){
@@ -391,6 +479,14 @@ indietro(){
     
   }
 
+  confronta(){
+    
+      this.getColoriDaSensore();
+      this.rgbNew = [this.codiceR, this.codiceG, this.codiceB];
+      
+      
+    }
+    
 
 
   reset(){
